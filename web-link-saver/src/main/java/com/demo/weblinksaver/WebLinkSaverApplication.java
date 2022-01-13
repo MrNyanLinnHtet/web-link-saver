754a7a9bc1f79a67910502be7ac84b821ebf4991
MrNyanLinnHtet <mr.nyanlinnhtet13@gmail.com>
2022-01-13 15:12:49 +0630

    creation project c0

---
+++ web-link-saver/src/main/java/com/demo/weblinksaver/WebLinkSaverApplication.java
@@ -0,0 +1,13 @@
+package com.demo.weblinksaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLinkSaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLinkSaverApplication.class, args);
+    }
+
+}
---
+++ web-link-saver/src/test/java/com/demo/weblinksaver/WebLinkSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.weblinksaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLinkSaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
